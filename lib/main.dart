def9c10d2233b3efdcc813857b7c1b2e45047c2e
Mohamad Valizade <mohammad.valizadeh@firstsource.io>
2020-10-20 18:34:15 +0330

    clean project

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:crowdintest/generated/l10n.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
@@ -10,12 +9,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      localizationsDelegates: [
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-        Localization.delegate
-      ],
-      supportedLocales: Localization.delegate.supportedLocales,
+      // localizationsDelegates: [
+      //   GlobalMaterialLocalizations.delegate,
+      //   GlobalWidgetsLocalizations.delegate,
+      //   Localization.delegate
+      // ],
+      // supportedLocales: Localization.delegate.supportedLocales,
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
@@ -47,27 +46,42 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(Localization.of(context).title),
+        title: Text('Localization.of(context).title'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              Localization.of(context).description,
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              IconButton(
+                tooltip: 'Change Language!',
+                color: Colors.black45,
+                icon: Icon(Icons.language),
+                onPressed: () => _changeLanguage(),
+              )
+            ],
+          ),
+          Spacer(),
+          Text(
+            'Localization.of(context).description',
+          ),
+          Text(
+            '$_counter',
+            style: Theme.of(context).textTheme.headline4,
+          ),
+          Spacer(),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
-        tooltip: Localization.of(context).fabTooltip,
+        tooltip: 'Localization.of(context).fabTooltip',
         child: Icon(Icons.add),
       ),
     );
+  }
+
+  void _changeLanguage() {
+    return null;
   }
 }
